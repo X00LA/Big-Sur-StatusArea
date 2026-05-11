0b665f8764dc382056cfc167db233b305535e541
X00LA <xoolalp@gmail.com>
2026-05-11 20:19:11 +0000

    Refactor extension.js to use ES6 modules

--- extension.js
+++ extension.js
@@ -16,198 +16,194 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-const GObject = imports.gi.GObject;
-const Main = imports.ui.main;
-const Config = imports.misc.config;
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const Convenience = Me.imports.convenience;
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
-const MenuItems = Me.imports.menuItems.MenuItems;
-const CustomButton = Me.imports.indicators.button.CustomButton;
-const CalendarIndicator = Me.imports.indicators.calendar.CalendarIndicator;
-const NetworkIndicator = Me.imports.indicators.network.NetworkIndicator;
-const BluetoothIndicator = Me.imports.indicators.bluetooth.BluetoothIndicator;
-const NightLightIndicator = Me.imports.indicators.nightlight.NightLightIndicator;
-const LightIndicator = Me.imports.indicators.light.LightIndicator;
-const NotificationIndicator = Me.imports.indicators.notification.NotificationIndicator;
-const PowerIndicator = Me.imports.indicators.power.PowerIndicator;
-const UserIndicator = Me.imports.indicators.system.UserIndicator;
-const VolumeIndicator = Me.imports.indicators.volume.VolumeIndicator;
+import { MenuItems } from './menuItems.js';
+import { CalendarIndicator } from './indicators/calendar.js';
+import { NetworkIndicator } from './indicators/network.js';
+import { BluetoothIndicator } from './indicators/bluetooth.js';
+import { NightLightIndicator } from './indicators/nightlight.js';
+import { LightIndicator } from './indicators/light.js';
+import { NotificationIndicator } from './indicators/notification.js';
+import { PowerIndicator } from './indicators/power.js';
+import { UserIndicator } from './indicators/system.js';
+import { VolumeIndicator } from './indicators/volume.js';
 
-function init() {
-    Convenience.initTranslations("bigSur-StatusArea");
-}
+export default class PanelIndicatorsExtension extends Extension {
 
-let settings;
-let menuItems;
-let indicators = null;
-let settingsChanged;
+    enable() {
+        this._settings = this.getSettings();
+        this._menuItems = new MenuItems(this._settings);
+        this._indicators = null;
+        this._settingsChanged = [];
 
-let light = null;
-let nightlight = null;
-let volume = null;
-let network = null;
-let bluetooth = null;
-let power = null;
-let calendar = null;
-let user = null;
-let notification = null;
+        // Indikatoren instanziieren
+        this._network      = new NetworkIndicator();
+        this._bluetooth    = new BluetoothIndicator();
+        this._volume       = new VolumeIndicator();
+        this._power        = new PowerIndicator();
+        this._calendar     = new CalendarIndicator();
+        this._notification = new NotificationIndicator();
+        this._user         = new UserIndicator();
+        this._nightlight   = new NightLightIndicator();
+        this._light        = new LightIndicator();
 
-const CENTER_BOX = Main.panel._centerBox;
-const RIGHT_BOX = Main.panel._rightBox;
+        // Originale GNOME-Indikatoren ausblenden
+        Main.panel.statusArea.aggregateMenu.hide();
+        Main.panel.statusArea.dateMenu.hide();
+        Main.panel._centerBox.remove_child(
+            Main.panel.statusArea.dateMenu.container
+        );
 
-function enable() {
-    Main.panel.statusArea.aggregateMenu.hide();
-    Main.panel.statusArea.dateMenu.hide();
-    Main.panel._centerBox.remove_child(Main.panel.statusArea.dateMenu.container);
+        // Indikatoren in die Panel-Statusarea eintragen
+        const addTo = (indicator) => {
+            if (indicator)
+                Main.panel.addToStatusArea(indicator.name, indicator, 0, 'right');
+        };
+        addTo(this._notification);
+        addTo(this._user);
+        addTo(this._calendar);
+        addTo(this._power);
+        addTo(this._network);
+        addTo(this._bluetooth);
+        addTo(this._volume);
+        addTo(this._nightlight);
+        addTo(this._light);
 
-    network = new NetworkIndicator();
-    bluetooth = new BluetoothIndicator();
-    volume = new VolumeIndicator();
-    power = new PowerIndicator();
-    calendar = new CalendarIndicator();
-    notification = new NotificationIndicator();
-    user = new UserIndicator();
-    nightlight = new NightLightIndicator();
-    light = new LightIndicator();
-    
-    if (notification)
-       Main.panel.addToStatusArea(notification.name, notification, 0, "right");
-    if (user)
-       Main.panel.addToStatusArea(user.name, user, 0, "right");
-    if (calendar)
-       Main.panel.addToStatusArea(calendar.name, calendar, 0, "right");
-    if (power)
-       Main.panel.addToStatusArea(power.name, power, 0, "right");
-    if (network)
-       Main.panel.addToStatusArea(network.name, network, 0, "right");
-    if (bluetooth)
-       Main.panel.addToStatusArea(bluetooth.name, bluetooth, 0, "right");
-    if (bluetooth)
-       Main.panel.addToStatusArea(volume.name, volume, 0, "right");
-    if (nightlight)
-       Main.panel.addToStatusArea(nightlight.name, nightlight, 0, "right");
-    if (light)
-       Main.panel.addToStatusArea(light.name, light, 0, "right");
+        // Settings-Listener
+        let i = 0;
+        this._settingsChanged[i++] = this._settings.connect('changed::items',                           () => this._applySettings());
+        this._settingsChanged[i++] = this._settings.connect('changed::spacing',                         () => this._applySettings());
+        this._settingsChanged[i++] = this._settings.connect('changed::user-icon',                       () => this._changeUsericon());
+        this._settingsChanged[i++] = this._settings.connect('changed::date-format',                     () => this._changeDateformat());
+        this._settingsChanged[i++] = this._settings.connect('changed::activate-spacing',                () => this._applySettings());
+        this._settingsChanged[i++] = this._settings.connect('changed::separate-date-and-notification',  () => this._applySettings());
 
-    // Load Settings
-    settings = Convenience.getSettings();
-    menuItems = new MenuItems(settings);
-    settingsChanged = new Array();
-    let i = 0;
-    settingsChanged[i++] = settings.connect("changed::items", applySettings);
-    settingsChanged[i++] = settings.connect("changed::spacing", applySettings);
-    settingsChanged[i++] = settings.connect("changed::user-icon", changeUsericon);
-    settingsChanged[i++] = settings.connect("changed::date-format", changeDateformat);
-    settingsChanged[i++] = settings.connect("changed::activate-spacing", applySettings);
-    settingsChanged[i++] = settings.connect("changed::separate-date-and-notification", applySettings);
+        this._applySettings();
+        this._changeUsername();
+        this._changeUsericon();
+        this._changeDateformat();
+    }
 
-    applySettings();
-    changeUsername();
-    changeUsericon();
-    changeDateformat();
-}
+    disable() {
+        // Settings-Listener trennen
+        this._settingsChanged.forEach(id => this._settings.disconnect(id));
+        this._settingsChanged = null;
+        this._settings = null;
 
-function changeUsername() {
-    let username = "";
-    user.changeLabel(username);
-}
+        // Indikatoren aufräumen
+        this._light.destroy();
+        this._nightlight.destroy();
+        this._volume.destroy();
+        this._power.destroy();
+        this._network.destroy();
+        this._bluetooth.destroy();
+        this._user.destroy();
+        this._notification.destroy();
+        this._calendar.destroy();
 
-function changeUsericon() {
-    let enableUserIcon = settings.get_boolean("user-icon");
-    user.changeIcon(enableUserIcon);
-}
+        this._light        = null;
+        this._nightlight   = null;
+        this._volume       = null;
+        this._power        = null;
+        this._network      = null;
+        this._bluetooth    = null;
+        this._user         = null;
+        this._notification = null;
+        this._calendar     = null;
+        this._menuItems    = null;
+        this._indicators   = null;
 
-function changeDateformat() {
-    let dateformat = settings.get_string("date-format");
-    calendar.override(dateformat);
-}
+        // Originale GNOME-Indikatoren wiederherstellen
+        Main.panel.statusArea.aggregateMenu.container.show();
+        Main.panel.statusArea.dateMenu.container.show();
+        Main.panel._centerBox.add_child(
+            Main.panel.statusArea.dateMenu.container
+        );
+    }
 
-function applySettings() {
-    let enabled = menuItems.getEnableItems();
-    let center = menuItems.getCenterItems();
-    indicators = new Array(enabled.length);
+    _changeUsername() {
+        this._user.changeLabel('');
+    }
 
-    removeAll();
-    setup(enabled, center, indicators, "power", power);
-    setup(enabled, center, indicators, "user", user);
-    setup(enabled, center, indicators, "volume", volume);
-    setup(enabled, center, indicators, "network", network);
-    setup(enabled, center, indicators, "bluetooth", bluetooth);
-    setup(enabled, center, indicators, "calendar", calendar);
-    setup(enabled, center, indicators, "notification", notification);
-    setup(enabled, center, indicators, "nightlight", nightlight);
-    setup(enabled, center, indicators, "light", light);
+    _changeUsericon() {
+        const enableUserIcon = this._settings.get_boolean('user-icon');
+        this._user.changeIcon(enableUserIcon);
+    }
 
-    let rightchildren = RIGHT_BOX.get_children().length;
-    let centerchildren = CENTER_BOX.get_children().length;
+    _changeDateformat() {
+        const dateformat = this._settings.get_string('date-format');
+        this._calendar.override(dateformat);
+    }
 
-    let spacing = settings.get_int("spacing");
-    if (!settings.get_boolean("activate-spacing"))
-	spacing = -1;
+    _applySettings() {
+        const CENTER_BOX = Main.panel._centerBox;
+        const RIGHT_BOX  = Main.panel._rightBox;
 
-    indicators.reverse().forEach(function (item) {
-        item.set_spacing(spacing);
-        if (item._center) {
-            CENTER_BOX.insert_child_at_index(item.container, centerchildren);
-        } else {
-            RIGHT_BOX.insert_child_at_index(item.container, rightchildren);
+        const enabled = this._menuItems.getEnableItems();
+        const center  = this._menuItems.getCenterItems();
+        this._indicators = new Array(enabled.length);
+
+        this._removeAll();
+
+        this._setup(enabled, center, 'power',        this._power);
+        this._setup(enabled, center, 'user',         this._user);
+        this._setup(enabled, center, 'volume',       this._volume);
+        this._setup(enabled, center, 'network',      this._network);
+        this._setup(enabled, center, 'bluetooth',    this._bluetooth);
+        this._setup(enabled, center, 'calendar',     this._calendar);
+        this._setup(enabled, center, 'notification', this._notification);
+        this._setup(enabled, center, 'nightlight',   this._nightlight);
+        this._setup(enabled, center, 'light',        this._light);
+
+        const rightchildren  = RIGHT_BOX.get_children().length;
+        const centerchildren = CENTER_BOX.get_children().length;
+
+        let spacing = this._settings.get_int('spacing');
+        if (!this._settings.get_boolean('activate-spacing'))
+            spacing = -1;
+
+        this._indicators.reverse().forEach(item => {
+            item.set_spacing(spacing);
+            if (item._center)
+                CENTER_BOX.insert_child_at_index(item.container, centerchildren);
+            else
+                RIGHT_BOX.insert_child_at_index(item.container, rightchildren);
+        });
+    }
+
+    _setup(enabledItems, centerItems, name, indicator) {
+        if (!indicator) return;
+        const index = enabledItems.indexOf(name);
+        if (index !== -1) {
+            this._indicators[index] = indicator;
+            this._indicators[index]._center = centerItems.indexOf(name) !== -1;
         }
-    });
-
-}
-
-function setup(enabledItems, centerItems, arrayIndicators, name, indicator) {
-    if (!indicator) return;
-    let index = enabledItems.indexOf(name);
-    let valid = index != -1;
-    if (valid) {
-        arrayIndicators[index] = indicator;
-        arrayIndicators[index]._center = centerItems.indexOf(name) != -1;
     }
-}
 
-function removeAll() {
-    removeContainer(light);
-    removeContainer(nightlight);
-    removeContainer(volume);
-    removeContainer(network);
-    removeContainer(bluetooth);
-    removeContainer(power);
-    removeContainer(calendar);
-    removeContainer(user);
-    removeContainer(notification);
-}
-
-function removeContainer(item) {
-    if (!item) return;
-    if (item._center) {
-        CENTER_BOX.remove_child(item.container)
-    } else {
-        RIGHT_BOX.remove_child(item.container);
+    _removeAll() {
+        [
+            this._light,
+            this._nightlight,
+            this._volume,
+            this._network,
+            this._bluetooth,
+            this._power,
+            this._calendar,
+            this._user,
+            this._notification,
+        ].forEach(item => this._removeContainer(item));
     }
-    item._center = false;
-}
 
-function disable() {
-    settingsChanged.forEach(function (item) {
-        settings.disconnect(item);
-    });
-    settingsChanged = null;
-    settings = null;
-
-    light.destroy();
-    nightlight.destroy();
-    volume.destroy();
-    power.destroy();
-    network.destroy();
-    bluetooth.destroy();
-    user.destroy();
-    notification.destroy();
-    calendar.destroy();
-
-    Main.panel.statusArea.aggregateMenu.container.show();
-    Main.panel.statusArea.dateMenu.container.show();
-    Main.panel._centerBox.add_child(Main.panel.statusArea.dateMenu.container);
+    _removeContainer(item) {
+        if (!item) return;
+        const box = item._center ? Main.panel._centerBox : Main.panel._rightBox;
+        try {
+            box.remove_child(item.container);
+        } catch (_) {
+            // Container war nicht in dieser Box — ignorieren
+        }
+        item._center = false;
+    }
 }
